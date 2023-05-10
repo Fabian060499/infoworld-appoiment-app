486f98465b130b3d614a14845282e4d525a74997
Fabian060499 <barbufaby@yahoo.ro>
2023-05-10 17:38:50 +0300

    Added appointment table and implement the move and cancel logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppointmentService } from './appointment.service';
+import { Appointment } from './appointment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,54 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'infoworld-appoiment-app';
+  public appArray: Appointment[] = [];
+  public selectedAppointment: Appointment | null = null;
+  
+  constructor(private appointmentService: AppointmentService) {
+    this.appointmentService.getAppointments().subscribe({
+      next: (response) => {
+        console.log(response);
+        this.appArray = response;
+      },
+      error: (err) => {
+        console.warn(err)
+      }
+    });
+  }
+
+  // Function to handle the slot selection
+  selectSlot(appointment: Appointment) {
+    // Only allow selection of scheduled appointments
+    if (appointment.status === 'Scheduled') {
+      this.selectedAppointment = appointment;
+    }
+  }
+  //Function to cancel appointment
+  cancelAppointment(appointment: Appointment) {
+    appointment.status = "Cancelled";
+  }
+  // Function to handle the appointment move
+  /**
+   * This function allows the user to move a scheduled appointment to a different time by swapping the
+   * appointment times with another scheduled appointment.
+   * @param {Appointment} targetAppointment - Appointment object representing the appointment that the
+   * selected appointment will be swapped with.
+   */
+  moveAppointment(targetAppointment: Appointment) {
+    // targetAppointment.appointmentTime=new Date(targetAppointment.appointmentTime);
+    // Only allow move of scheduled appointments
+    if (this.selectedAppointment && this.selectedAppointment.status === 'Scheduled') {
+      // Swap the appointments
+      const tempTime = this.selectedAppointment.appointmentTime;
+      this.selectedAppointment.appointmentTime = targetAppointment.appointmentTime;
+      targetAppointment.appointmentTime = tempTime;
+      
+      // Clear the selected appointment
+      this.selectedAppointment = null;
+      
+      // Sort the appointments by time
+      this.appArray.sort((a, b) => a.appointmentTime.getTime() - b.appointmentTime.getTime());
+    }
+  }
 }
+
